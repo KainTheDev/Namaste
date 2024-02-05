763d8bbf4bc396fe4e24d674872127414e7ab55b
KainTheDev <thica.doi@gmail.com>
2024-02-05 10:00:23 +0700

    .

--- public/global.js
+++ public/global.js
@@ -5,10 +5,6 @@ let condition = false
 const box = document.getElementsByClassName('box').item(0)
 box.style.opacity = '0'
 box.style.pointerEvents = 'none'
-window.addEventListener('focus', () => {
-    box.style.opacity = '1'
-    box.style.pointerEvents = ''
-}, false)
 
 setTimeout(() => { box.style.opacity = '1', box.style.pointerEvents = '' }, 1000)
 
