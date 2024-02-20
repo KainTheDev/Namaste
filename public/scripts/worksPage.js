5aec4985d83d8e9b89b4a1b37c15575c7134b063
KainTheDev <thica.doi@gmail.com>
2024-02-20 18:39:54 +0700

    .

--- public/scripts/worksPage.js
+++ public/scripts/worksPage.js
@@ -1,6 +1,6 @@
 const box = document.getElementsByClassName('box').item(0)
 fetch('json/works.json').then(async response => {
-    const data = await response.json()
+    const data = (await response.json()).sort((a, b) => new Number(b.creationYear) - new Number(a.creationYear));
     for (const work of data) {
         box.innerHTML += `
         <button class="imageButton" onclick="setTimeout(() => window.open('${work.link}', '_blank'), 500)" style="--preview-image: url('${work.backgroundImage}');">
