792debf63811a5a4f60ebee91c7900ca13be64d4
KainTheDev <thica.doi@gmail.com>
2024-03-05 08:11:16 +0700

    .

--- public/scripts/worksPage.js
+++ public/scripts/worksPage.js
@@ -2,10 +2,19 @@ const box = document.getElementsByClassName('box').item(0)
 fetch('json/works.json').then(async response => {
     const data = (await response.json()).sort((a, b) => new Number(b.creationYear) - new Number(a.creationYear));
     for (const work of data) {
-        box.innerHTML += `
-        <button class="imageButton" onclick="setTimeout(() => window.open('${work.link}', '_blank'), 500)" style="--preview-image: url('${work.backgroundImage}');">
-            <h1>${work.name}</h1>
-            <h2>${work.description}. <span class='highlight'><br>Creation year: <span class='highlight important'>${work.creationYear}</span>.</span></h2>
-        </button>`
+        const imageButton = document.createElement('button');
+        imageButton.className = 'imageButton';
+        imageButton.onclick = () => setTimeout(() => window.open(work.link, '_blank'), 500);
+        imageButton.style.setProperty('--preview-image', `url('${work.backgroundImage}')`);
+
+        const h1 = document.createElement('h1');
+        h1.textContent = work.name;
+
+        const h2 = document.createElement('h2');
+        h2.innerHTML = `${work.description}. <span class='highlight'><br>Creation year: <span class='highlight important'>${work.creationYear}</span>.</span>`;
+
+        imageButton.appendChild(h1);
+        imageButton.appendChild(h2);
+        box.insertBefore(imageButton, box.firstChild)
     }
 })
